f85002565f95546cc0630739012b986489129480
Yauhescha <evgescha.maslov007@yandex.ru>
2023-10-04 16:27:27 +0300

    add more enemy animations

--- core/src/com/hescha/game/util/EnemyBuilder.java
+++ core/src/com/hescha/game/util/EnemyBuilder.java
@@ -20,6 +20,44 @@ public class EnemyBuilder {
             new Texture("enemy/fat3/fat3 (1).png"),
             new Texture("enemy/fat3/fat3 (3).png")
     };
+
+    private static final Texture[] idleMan1Textures = {
+            new Texture("enemy/man1/1.png"),
+            new Texture("enemy/man1/2.png")
+    };
+
+    private static final Texture[] idleMan2Textures = {
+            new Texture("enemy/man2/1.png"),
+            new Texture("enemy/man2/2.png")
+    };
+
+    private static final Texture[] idleMan3Textures = {
+            new Texture("enemy/man3/1.png"),
+            new Texture("enemy/man3/2.png")
+    };
+
+    private static final Texture[] idleMan4Textures = {
+            new Texture("enemy/man4/1.png"),
+            new Texture("enemy/man4/2.png")
+    };
+
+
+    private static final Texture[] idleWoman1Textures = {
+            new Texture("enemy/woman1/1.png"),
+            new Texture("enemy/woman1/2.png")
+    };
+    private static final Texture[] idleWoman2Textures = {
+            new Texture("enemy/woman2/1.png"),
+            new Texture("enemy/woman2/2.png")
+    };
+    private static final Texture[] idleWoman3Textures = {
+            new Texture("enemy/woman3/1.png"),
+            new Texture("enemy/woman3/2.png")
+    };
+    private static final Texture[] idleWoman4Textures = {
+            new Texture("enemy/woman4/1.png"),
+            new Texture("enemy/woman4/2.png")
+    };
     public static final float FRAME_DURATION = 0.3f;
 
 
@@ -68,9 +106,73 @@ public class EnemyBuilder {
         return enemy;
     }
 
+    public static Enemy buildEnemyMan1() {
+        Animation<Texture> idleAnimation = new Animation<>(FRAME_DURATION, idleMan1Textures);
+        idleAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        Enemy enemy = new Enemy(idleAnimation, randomHair());
+
+        return enemy;
+    }
+
+    public static Enemy buildEnemyMan2() {
+        Animation<Texture> idleAnimation = new Animation<>(FRAME_DURATION, idleMan2Textures);
+        idleAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        Enemy enemy = new Enemy(idleAnimation, randomHair());
+
+        return enemy;
+    }
+
+    public static Enemy buildEnemyMan3() {
+        Animation<Texture> idleAnimation = new Animation<>(FRAME_DURATION, idleMan3Textures);
+        idleAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        Enemy enemy = new Enemy(idleAnimation, randomHair());
+
+        return enemy;
+    }
+
+    public static Enemy buildEnemyMan4() {
+        Animation<Texture> idleAnimation = new Animation<>(FRAME_DURATION, idleMan4Textures);
+        idleAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        Enemy enemy = new Enemy(idleAnimation, randomHair());
+
+        return enemy;
+    }
+
+    public static Enemy buildEnemyWoman1() {
+        Animation<Texture> idleAnimation = new Animation<>(FRAME_DURATION, idleWoman1Textures);
+        idleAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        Enemy enemy = new Enemy(idleAnimation, randomHair());
+
+        return enemy;
+    }
+
+    public static Enemy buildEnemyWoman2() {
+        Animation<Texture> idleAnimation = new Animation<>(FRAME_DURATION, idleWoman2Textures);
+        idleAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        Enemy enemy = new Enemy(idleAnimation, randomHair());
+
+        return enemy;
+    }
+
+    public static Enemy buildEnemyWoman3() {
+        Animation<Texture> idleAnimation = new Animation<>(FRAME_DURATION, idleWoman3Textures);
+        idleAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        Enemy enemy = new Enemy(idleAnimation, randomHair());
+
+        return enemy;
+    }
+
+    public static Enemy buildEnemyWoman4() {
+        Animation<Texture> idleAnimation = new Animation<>(FRAME_DURATION, idleWoman4Textures);
+        idleAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        Enemy enemy = new Enemy(idleAnimation, randomHair());
+
+        return enemy;
+    }
+
 
     public static Enemy randomBuildEnemy() {
-        int randomNumber = random.nextInt(3);
+        int randomNumber = random.nextInt(11);
 
         switch (randomNumber) {
             case 0:
@@ -79,6 +181,22 @@ public class EnemyBuilder {
                 return buildEnemyFat2();
             case 2:
                 return buildEnemyFat3();
+            case 3:
+                return buildEnemyMan1();
+            case 4:
+                return buildEnemyMan2();
+            case 5:
+                return buildEnemyMan3();
+            case 6:
+                return buildEnemyMan4();
+            case 7:
+                return buildEnemyWoman1();
+            case 8:
+                return buildEnemyWoman2();
+            case 9:
+                return buildEnemyWoman3();
+            case 10:
+                return buildEnemyWoman4();
             default:
                 return buildEnemyFat1();
         }
